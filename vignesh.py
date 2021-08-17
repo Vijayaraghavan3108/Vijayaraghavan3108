b19646d7d957e219646d1db2fd6bec95a071c623
Vijayaraghavan3108 <89076760+Vijayaraghavan3108@users.noreply.github.com>
2021-08-17 16:18:23 +0530

    Add files via upload

---
+++ vignesh.py
@@ -0,0 +1,19 @@
+def linear search  (key):
+    a=[1,2,3,4,5,6]
+    for i in range(len(a));
+    if a [i]==key;
+    print ["key found"]
+    else:
+        print("key not found")
+        linear search(4)
+
+
+
+
+
+
+
+
+
+
+                                                                                                                               
